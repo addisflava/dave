f647a0c9215774d6ced9dc0fa01c6f8727221396
addisflava <danielmelake@gmail.com>
2015-10-07 20:34:34 -0400

    commit

---
+++ app/build/generated/source/buildConfig/release/github/daneren2005/dsub/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package github.daneren2005.dsub;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "github.daneren2005.dsub";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 158;
+  public static final String VERSION_NAME = "";
+}
